a91e8b0d0ba9d4a866ffac502489edb4f6113118
jppars <joshpparsons@gmail.com>
2023-12-11 21:17:04 -0500

    removed .txt files and updated gitignores

---
+++ 2023/day12.py
@@ -0,0 +1,17 @@
+import re
+
+def part1(f):
+    test_input = f.read()
+
+    return 0
+
+def part2(f):
+    test_input = f.read()
+
+    return 0
+
+with open('inputs/day12_input.txt', 'r') as f:
+    print('Results from Part 1: {}'.format(part1(f)))
+
+with open('inputs/day12_input.txt', 'r') as f:
+    print('Results from Part 2: {}'.format(part2(f)))
